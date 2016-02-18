635282c86da51f9965293185794d81152b76d5ce
kaira2006 <kayura60@gmail.com>
2016-02-18 14:10:31 -0600

    First Steps!
    Create a scene with three locations.  Started work on the character
    modeling script. Will need to link affects to needs and locations.

---
+++ TotalWorldDomination/Assets/CharacterModel.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterModel : MonoBehaviour {
+
+
+	[Range(-1.0f, 1.0f)]
+	public float hunger, social, sleep;
+
+	public float hungerRate;
+
+	[Range (0.0f, 1.0f)]
+	public float deltaAffectOne, deltaAffectTwo, deltaAffectThree;
+	
+
+	[Range(-1.0f, 1.0f)]
+	public float affectOne, affectTwo, affectThree;
+
+	private int signA1, signA2, signA3;
+
+	public Lunchroom lunchroom;
+	public GameObject danceClub;
+	 
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		signA1 = 1;
+		signA2 = -1;
+		signA3 = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		hunger = Mathf.Clamp (hunger - hungerRate * Time.deltaTime, -1, 1);
+		if (hunger < -.5) {
+			this.transform.position = lunchroom.transform.position;
+			lunchroom.EnterLunchroom (this);
+
+		} else {
+			this.transform.position = danceClub.transform.position;
+			lunchroom.ExitLunchroom (this);
+		}
+
+
+
+		affectOne = Mathf.Clamp (affectOne + signA1 * deltaAffectOne * Time.deltaTime, -1, 1);
+		affectTwo = Mathf.Clamp (affectTwo + signA2 * deltaAffectTwo * Time.deltaTime, -1, 1);
+		affectThree = Mathf.Clamp (affectThree + signA3 * deltaAffectThree * Time.deltaTime, -1, 1);
+	}
+}
---
+++ TotalWorldDomination/Assets/Lunchroom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lunchroom : MonoBehaviour {
+
+	public List<CharacterModel> characters = new List<CharacterModel>();
+	public float foodRate;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach (CharacterModel c in characters) {
+
+			c.hunger += foodRate;
+		}
+
+
+
+	}
+
+	public void EnterLunchroom(CharacterModel c){
+
+		characters.Add (c);
+
+	}
+
+	public void ExitLunchroom(CharacterModel c){
+
+		characters.Remove (c);
+	}
+}

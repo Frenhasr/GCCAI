7210eb99af50b5ddb9e6fede01e26e7415df7e4c
kaira2006 <kayura60@gmail.com>
2016-04-19 17:21:29 -0500

    New Affect Class
    Please do not confuse this with the AFFECTS class.  Will update this
    soon. sorry….

---
+++ TotalWorldDomination/Assets/Scripts/Affect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Affect
+{
+
+	private string name;
+	private float lowerBound, upperBound, neutral, currentVal;
+
+	public  Affect (string name, float lowerBound, float upperBound, float neutral, float currentVal)
+	{
+
+		this.name = name;
+		this.lowerBound = lowerBound;
+		this.upperBound = upperBound;
+		this.neutral = neutral;
+		this.currentVal = currentVal;
+
+	}
+
+	public float LowerBound ()
+	{
+
+		return lowerBound;
+
+	}
+
+	public float UpperBound ()
+	{
+		return upperBound;
+
+	}
+
+	public float NeutralBound ()
+	{
+
+		return neutral;
+	}
+
+
+	public float CurrentVal ()
+	{
+
+		return currentVal;
+	}
+
+
+	public void ValueChange (float v)
+	{
+
+		currentVal += v;
+
+
+	}
+
+
+}
